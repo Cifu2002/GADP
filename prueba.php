a49c9ca245ca5ccd4d6866a97efcfd60815e2fbd
Cifu2002 <estebancifu88@gmail.com>
2024-08-16 16:56:01 -0500

    i8

--- prueba.php
+++ prueba.php
@@ -1,7 +1,16 @@
 <?php
 $ip = isset($_GET['ip']) ? $_GET['ip'] : 'No disponible';
 $mac = isset($_GET['mac']) ? $_GET['mac'] : 'No disponible';
-
-// Redirigir a la página deseada con los datos obtenidos
-exit;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>IP: <?php echo htmlspecialchars($ip); ?></h1>
+    <h1>MAC: <?php echo htmlspecialchars($mac); ?></h1>
+</body>
+</html>
