1d8eff914aded3d4b3ee4e5fd8411070c9b5ba2f
Cifu2002 <estebancifu88@gmail.com>
2024-08-16 16:20:13 -0500

    i5

--- prueba.php
+++ prueba.php
@@ -1,4 +1,12 @@
 <?php
-include_once("conexion.php");
-$conexion = Conexion::getInstance()->getConexion();
+// Obtener IP de la máquina
+$ip = getHostByName(getHostName());
+
+// Obtener MAC de la máquina (ejemplo para Windows)
+$mac = shell_exec("getmac /v /fo list | findstr /C:\"Dirección física\"");
+$mac = trim($mac);
+
+// Mostrar IP y MAC
+echo "IP: " . $ip . "<br>";
+echo "MAC: " . $mac . "<br>";
 ?>
