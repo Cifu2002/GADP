edb06ebfcacbed6719f23e1fb923eac29bc795d3
Cifu2002 <estebancifu88@gmail.com>
2024-08-16 16:49:47 -0500

    i7

--- prueba.php
+++ prueba.php
@@ -1,32 +1,7 @@
 <?php
-// Obtener IP de la máquina
-$ip = getHostByName(getHostName());
+$ip = isset($_GET['ip']) ? $_GET['ip'] : 'No disponible';
+$mac = isset($_GET['mac']) ? $_GET['mac'] : 'No disponible';
 
-// Obtener MAC de la máquina (ejemplo para Windows)
-$mac = shell_exec("getmac /v /fo list | findstr /C:\"Dirección física\"");
-$mac = trim($mac);
-
-// Mostrar IP y MAC
-echo "IP: " . $ip . "<br>";
-echo "MAC: " . $mac . "<br>";
+// Redirigir a la página deseada con los datos obtenidos
+exit;
 ?>
-<script>
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            console.log("IP Pública: " + data.ip);
-        });
-</script>
-<script>
-    const pc = new RTCPeerConnection();
-    pc.createDataChannel("");
-    pc.createOffer(pc.setLocalDescription.bind(pc), () => {});
-
-    pc.onicecandidate = function(event) {
-        if (event.candidate) {
-            const ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/;
-            const ip_address = ip_regex.exec(event.candidate.candidate)[1];
-            console.log("IP Local: " + ip_address);
-        }
-    };
-</script>
