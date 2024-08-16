eaafb8a951e9f28bb77fd7f80dd221e75a9b2ee6
Cifu2002 <estebancifu88@gmail.com>
2024-08-16 16:25:36 -0500

    i6

--- prueba.php
+++ prueba.php
@@ -10,3 +10,23 @@ $mac = trim($mac);
 echo "IP: " . $ip . "<br>";
 echo "MAC: " . $mac . "<br>";
 ?>
+<script>
+    fetch('https://api.ipify.org?format=json')
+        .then(response => response.json())
+        .then(data => {
+            console.log("IP Pública: " + data.ip);
+        });
+</script>
+<script>
+    const pc = new RTCPeerConnection();
+    pc.createDataChannel("");
+    pc.createOffer(pc.setLocalDescription.bind(pc), () => {});
+
+    pc.onicecandidate = function(event) {
+        if (event.candidate) {
+            const ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3})/;
+            const ip_address = ip_regex.exec(event.candidate.candidate)[1];
+            console.log("IP Local: " + ip_address);
+        }
+    };
+</script>
