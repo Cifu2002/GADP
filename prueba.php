2f931cb1b14c6de2038c9783f7c0e850edfe0341
Cifu2002 <estebancifu88@gmail.com>
2024-08-16 08:12:46 -0500

    conexion 5

--- prueba.php
+++ prueba.php
@@ -1,18 +1,16 @@
 <?php
-
 class Conexion
 {
-    private static $instance = null; // Instancia única de la clase
-    private $conexion; // Objeto de conexión a la base de datos
+    private static $instance;
+    private $conexion;
 
-    // Constructor privado para evitar la creación de instancias desde fuera de la clase
     private function __construct() {
         $db_username = 'ERPTENA';
         $db_password = 'GADTN$$2022';
         $db_connection_string = 'cabildo';
 
         try {
-            // Usando OCI8 para conectar a Oracle
+            // Usando OCI8 para la conexión
             $this->conexion = oci_connect($db_username, $db_password, $db_connection_string);
             if (!$this->conexion) {
                 $e = oci_error();
@@ -23,32 +21,27 @@ class Conexion
         }
     }
 
-    // Método estático para obtener la instancia única de la clase
-    public static function getInstance()
-    {
-        if (self::$instance === null) {
-            self::$instance = new self(); // Crear una nueva instancia si no existe
-        }
-        return self::$instance;
-    }
-
-    // Método para obtener la conexión
-    public function getConexion()
-    {
-        return $this->conexion;
-    }
-
-    // Destructor para cerrar la conexión cuando la instancia sea destruida
     public function __destruct() {
         if ($this->conexion) {
             oci_close($this->conexion);
         }
     }
 
-    // Método privado para evitar la clonación de la instancia
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getConexion()
+    {
+        return $this->conexion;
+    }
+
     private function __clone()
     {
     }
 }
-
 ?>
