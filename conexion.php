7dd61df423d9928c739237d32b6d7ecfee58c1f0
Cifu2002 <estebancifu88@gmail.com>
2024-08-19 09:50:46 -0500

    2

--- conexion.php
+++ conexion.php
@@ -8,25 +8,17 @@ class Conexion
         $username = 'ERPTENA';
         $password = 'GADTN$$2022';
         $connection_string = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=172.16.66.2)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=TENA)))';
-    
+
         try {
-            // Conectar a Oracle
             $this->conexion = oci_connect($username, $password, $connection_string);
-    
             if (!$this->conexion) {
                 $e = oci_error();
                 throw new Exception($e['message']);
             }
-    
-            // Establecer la codificación a UTF-8
-            oci_set_client_encoding($this->conexion, 'AL32UTF8');
-    
         } catch (Exception $e) {
             die("Error al conectar a Oracle: " . $e->getMessage());
         }
     }
-    
-    
 
     public function __destruct() {
         if ($this->conexion) {
