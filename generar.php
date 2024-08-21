d1e4f80d1370fb8bdf2c4cd5427ad34b99402ee6
Cifu2002 <estebancifu88@gmail.com>
2024-08-21 11:52:56 -0500

    6

--- generar.php
+++ generar.php
@@ -1,26 +1,46 @@
 <?php
 include_once('pdf copy.php');
+$json_input = file_get_contents('php://input');
+$data = json_decode($json_input, true);
+$opP = filter_var($data['op'], FILTER_SANITIZE_STRING);
+$componentes = isset($data['componentes']) ? $data['componentes'] : [];
+$cambios = isset($data['cambios']) ? $data['cambios'] : [];
+$codigo = filter_var($data['codigo'], FILTER_SANITIZE_STRING);
+$mac = filter_var($data['mac'], FILTER_SANITIZE_STRING);
+$ip = filter_var($data['ip'], FILTER_SANITIZE_STRING);
+$tipoSolicitud = filter_var($data['tipoSolicitud'], FILTER_SANITIZE_STRING);
+$responsableBien = filter_var($data['responsableBien'], FILTER_SANITIZE_STRING);
+$departamento = filter_var($data['departamento'], FILTER_SANITIZE_STRING);
+$cedula = filter_var($data['cedula'], FILTER_SANITIZE_STRING);
+$cargo = filter_var($data['cargo'], FILTER_SANITIZE_STRING);
+$encargado = filter_var($data['encargado'], FILTER_SANITIZE_STRING);
+$fechaSolicitud = filter_var($data['fechaSolicitud'], FILTER_SANITIZE_STRING);
+$horaSolicitud = filter_var($data['horaSolicitud'], FILTER_SANITIZE_STRING);
+$fechaSolicitudF = filter_var($data['fechaSolicitudF'], FILTER_SANITIZE_STRING);
+$horaSolicitudF = filter_var($data['horaSolicitudF'], FILTER_SANITIZE_STRING);
+$detalles = filter_var($data['detalles'], FILTER_SANITIZE_STRING);
+$tipoMantenimiento = isset($data['tipoMantenimiento']) ? json_decode($data['tipoMantenimiento'], true) : [];
 
-$solicitudID = $_POST['solicitudID'];
-$codigo = $_POST['codigo'];
-$mac = $_POST['mac'];
-$ip = isset($_POST['ip']) ? $_POST['ip'] : ''; // Si no se pasa, puedes dejarlo vacío
-$tipoSolicitud = $_POST['tipoSolicitud'];
-$tipoMantenimientoString = $_POST['tipoMantenimientoString'];
-$responsableBien = $_POST['responsableBien'];
-$departamento = $_POST['departamento'];
-$cedula = isset($_POST['cedula']) ? $_POST['cedula'] : ''; // Si no se pasa, puedes dejarlo vacío
-$cargo = isset($_POST['cargo']) ? $_POST['cargo'] : ''; // Si no se pasa, puedes dejarlo vacío
-$encargado = $_POST['encargado'];
-$fechaSolicitud = $_POST['fechaSolicitud'];
-$horaSolicitud = $_POST['horaSolicitud'];
-$fechaSolicitudF = $_POST['fechaSolicitudF'];
-$horaSolicitudF = $_POST['horaSolicitudF'];
-$detalles = $_POST['detalles'];
-$impresoraString = $_POST['impresoraString'];
+$impresora = isset($data['impresora']) ? json_decode($data['impresora'], true) : [];
+$solicitudID = filter_var($data['solicitudID'], FILTER_SANITIZE_STRING);
+$tipoMantenimientoString = implode(',', $tipoMantenimiento);
+$impresoraString = implode(',', $impresora);
 
 // Llamada a la función GenerarPDFPreventivo
 PDF::GenerarPDFPreventivo(
-    
+    $solicitudID,
+    $codigo,
+    $mac,
+    $tipoSolicitud,
+    $tipoMantenimientoString,
+    $responsableBien,
+    $departamento,
+    $encargado,
+    $fechaSolicitud,
+    $horaSolicitud,
+    $fechaSolicitudF,
+    $horaSolicitudF,
+    $detalles,
+    $impresoraString
 );
 ?>
