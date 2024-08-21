ee911c04f8c03a4c992fdf42fc77e2737f8fde64
Cifu2002 <estebancifu88@gmail.com>
2024-08-21 14:59:29 -0500

    6

--- generar.php
+++ generar.php
@@ -1,5 +1,5 @@
 <?php
-include_once('pdf_copy.php');
+include_once('pdf copy.php');
 // Obtener los valores de la URL
 $op = isset($_GET['op']) ? $_GET['op'] : '';
 $solicitudID = isset($_GET['solicitudID']) ? $_GET['solicitudID'] : '';
@@ -28,7 +28,7 @@ $componentes = json_decode($componentes, true);
 $cambios = json_decode($cambios, true);
 $impresora = json_decode($impresora, true);
 // Generar PDF usando la función PDF::GenerarPDFPreventivo
-$pdfContent = PDF::GenerarPDFPreventivo(
+PDF::GenerarPDFPreventivo(
     $solicitudID,
     $codigo,
     $mac,
