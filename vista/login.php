63696e90aa8a5320dc1b921e3d891f8e1c296e05
Cifu2002 <estebancifu88@gmail.com>
2024-08-23 10:54:40 -0500

    6

--- vista/login.php
+++ vista/login.php
@@ -1,12 +1,5 @@
 <?php
-include_once('../modelo/Sesion.php');
-    $sesion = Sesion::getInstance();
 
-    if ($sesion->getSesion('usuario_id') && $sesion->getSesion('usuario_nombre')) {
-
-        header('Location: pagina-principal.html');
-        exit();
-    }
 ?>
 <!DOCTYPE html>
 <html lang="es">
