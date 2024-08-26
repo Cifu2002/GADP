715807d4aedb65dcdd729908c4d3618bc2537651
Cifu2002 <estebancifu88@gmail.com>
2024-08-26 14:29:58 -0500

    6

--- ReportePDF.php
+++ ReportePDF.php
@@ -3,7 +3,8 @@ require('assets/fpdf/fpdf.php');
 
 class PDF extends FPDF
 {
-    // Reporte de solicitudes
+
+    //Reporte de solicitudes
     public static function GenerarReportePDF($solicitudes)
     {
         // Crear instancia de PDF
@@ -59,7 +60,7 @@ class PDF extends FPDF
         $pdf->Output('D', 'Solicitudes.pdf');
     }
 
-    // Método para generar el PDF de solicitud Preventiva
+    /* Generar pdf de la solicitud Preventiva */
     public static function GenerarPDFPreventivo(
         $pdf,
         $solicitudID,
@@ -77,15 +78,83 @@ class PDF extends FPDF
         $detalles,
         $impresoraString
     ) {
-        // Generar el contenido del PDF para solicitudes preventivas
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(108, 10, 'Registro de asistencia / orden de trabajo Nro. Orden: ', 0, 0, 'L');
+
+        // Título principal
+        $pdf->Cell(108, 10, utf8_decode('Registro de asistencia / orden de trabajo Nro. Orden: '), 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(0, 10, utf8_decode($solicitudID), 0, 1, 'L');
-        // Continuar con la lógica específica para solicitudes preventivas...
+
+        $anchoPagina = $pdf->GetPageWidth();
+        $margenIzquierdo = $pdf->GetX();
+        $margenDerecho = $pdf->GetStringWidth(" ");
+        $anchoDisponible = $anchoPagina - $margenIzquierdo - $margenDerecho;
+        $anchoCelda = $anchoDisponible / 4;
+        $pdf->SetX($margenIzquierdo);
+
+        // Departamento
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(31, 10, 'Departamento: ', 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell($anchoCelda + 19, 10, utf8_decode($departamento), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Ln(10);
+
+        // Código y Mac
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(17, 10, utf8_decode('Código: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell($anchoCelda + 33, 10, utf8_decode($codigo), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(11, 10, utf8_decode('Mac: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell($anchoCelda, 10, utf8_decode($mac), 0, 1, 'L');
+
+        // Solicitud
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(20, 10, utf8_decode('Solicitud: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(0, 10, utf8_decode($tipoSolicitud), 0, 1, 'L');
+
+        // Tipo de mantenimiento e impresora
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(49, 10, utf8_decode('Tipo de mantenimiento: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(51, 10, utf8_decode($tipoMantenimientoString), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(43, 10, utf8_decode('Impresora Funciona: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(0, 10, utf8_decode($impresoraString), 0, 0, 'L');
+
+        // Detalles
+        $pdf->Ln(15);
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(0, 10, 'Detalles:', 0, 1, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->MultiCell(0, 5, utf8_decode($detalles), 0, 'L');
+
+        // Fechas
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(27, 10, utf8_decode('Fecha Inicio: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(30, 10, utf8_decode($fechaSolicitud), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(30, 10, utf8_decode('Hora de inicio: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(20, 10, utf8_decode($horaSolicitud), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(20, 10, utf8_decode('Fecha fin: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(33, 10, utf8_decode($fechaSolicitudF), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(18, 10, utf8_decode('Hora fin: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(0, 10, utf8_decode($horaSolicitudF), 0, 0, 'L');
+
+        
     }
 
-    // Método para generar el PDF de solicitud Correctiva
+    /* Generar pdf de la solicitud Correctiva */
     public static function GenerarPDFCorrectivo(
         $pdf,
         $solicitudID,
@@ -104,32 +173,85 @@ class PDF extends FPDF
         $componentes,
         $cambios
     ) {
-        // Generar el contenido del PDF para solicitudes correctivas
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(108, 10, 'Registro de asistencia / orden de trabajo Nro. Orden: ', 0, 0, 'L');
+
+        // Título principal
+        $pdf->Cell(108, 10, utf8_decode('Registro de asistencia / orden de trabajo Nro. Orden: '), 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(0, 10, utf8_decode($solicitudID), 0, 1, 'L');
+
+        $anchoPagina = $pdf->GetPageWidth();
+        $margenIzquierdo = $pdf->GetX();
+        $margenDerecho = $pdf->GetStringWidth(" ");
+        $anchoDisponible = $anchoPagina - $margenIzquierdo - $margenDerecho;
+        $anchoCelda = $anchoDisponible / 4;
+        $pdf->SetX($margenIzquierdo);
 
         // Departamento
         $pdf->SetFont('Arial', 'B', 12);
         $pdf->Cell(31, 10, 'Departamento: ', 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(0, 10, utf8_decode($departamento), 0, 1, 'L');
+        $pdf->Cell($anchoCelda + 19, 10, utf8_decode($departamento), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Ln(10);
 
         // Código y Mac
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(17, 10, 'Código: ', 0, 0, 'L');
+        $pdf->Cell(17, 10, utf8_decode('Código: '), 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(0, 10, utf8_decode($codigo), 0, 1, 'L');
-        $pdf->Cell(11, 10, 'Mac: ', 0, 0, 'L');
+        $pdf->Cell($anchoCelda + 33, 10, utf8_decode($codigo), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(11, 10, utf8_decode('Mac: '), 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
-        $pdf->Cell(0, 10, utf8_decode($mac), 0, 1, 'L');
+        $pdf->Cell($anchoCelda, 10, utf8_decode($mac), 0, 1, 'L');
 
         // Solicitud
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(20, 10, 'Solicitud: ', 0, 0, 'L');
+        $pdf->Cell(20, 10, utf8_decode('Solicitud: '), 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(0, 10, utf8_decode($tipoSolicitud), 0, 1, 'L');
+
+        // Impresora
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(43, 10, utf8_decode('Impresora Funciona: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(0, 10, utf8_decode($impresoraString), 0, 0, 'L');
+
+        // Componentes y Cambios 
+        $pdf->Ln(10);
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(100, 10, 'Componentes:', 0, 0, 'L');
+        $pdf->Cell(0, 10, 'Cambios:', 0, 1, 'L');
+
+        // Lista 
+        $pdf->SetFont('Arial', '', 12);
+
+        $maxLength = max(count($componentes), count($cambios));
+
+        for ($i = 0; $i < $maxLength; $i++) {
+            // Verificar si hay componentes
+            $componenteNombre = isset($componentes[$i]['nombre']) ? $componentes[$i]['nombre'] : null;
+            if ($componenteNombre !== null && trim($componenteNombre) !== '') {
+                $componenteText = '- ' . utf8_decode($componenteNombre);
+            } else {
+                $componenteText = null;
+            }
+
+            // Verificar si hay cambios
+            $cambioNombre = isset($cambios[$i]['nombreComponente']) ? $cambios[$i]['nombreComponente'] : null;
+            $cambioFecha = isset($cambios[$i]['fechaCambio']) ? $cambios[$i]['fechaCambio'] : null;
+            if ($cambioNombre !== null && trim($cambioNombre) !== '' && $cambioFecha !== null && trim($cambioFecha) !== '') {
+                $cambioText = '- ' . utf8_decode($cambioNombre);
+            } else {
+                $cambioText = null;
+            }
+
+            // Imprimir si hay texto válido para componentes o cambios
+            if ($componenteText || $cambioText) {
+                $pdf->Cell(100, 10, $componenteText ?: '- ', 0, 0, 'L');
+                $pdf->Cell(0, 10, $cambioText ?: '- ', 0, 1, 'L');
+            }
+        }
 
         // Detalles
         $pdf->SetFont('Arial', 'B', 12);
@@ -137,35 +259,24 @@ class PDF extends FPDF
         $pdf->SetFont('Arial', '', 12);
         $pdf->MultiCell(0, 5, utf8_decode($detalles), 0, 'L');
 
-        // Componentes
+        // Fechas
         $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(0, 10, 'Componentes:', 0, 1, 'L');
-        $pdf->SetFont('Arial', '', 12);
-        foreach ($componentes as $componente) {
-            $pdf->Cell(0, 10, utf8_decode($componente), 0, 1, 'L');
-        }
-
-        // Cambios
-        $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(0, 10, 'Cambios:', 0, 1, 'L');
-        $pdf->SetFont('Arial', '', 12);
-        foreach ($cambios as $cambio) {
-            $pdf->Cell(0, 10, utf8_decode($cambio), 0, 1, 'L');
-        }
-
-        // Fechas y Horas
-        $pdf->SetFont('Arial', 'B', 12);
-        $pdf->Cell(27, 10, 'Fecha Inicio: ', 0, 0, 'L');
+        $pdf->Cell(27, 10, utf8_decode('Fecha Inicio: '), 0, 0, 'L');
         $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(30, 10, utf8_decode($fechaSolicitud), 0, 0, 'L');
-        $pdf->Cell(30, 10, 'Hora de inicio: ', 0, 0, 'L');
-        $pdf->Cell(20, 10, utf8_decode($horaSolicitud), 0, 1, 'L');
-        $pdf->Cell(20, 10, 'Fecha fin: ', 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(30, 10, utf8_decode('Hora de inicio: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(20, 10, utf8_decode($horaSolicitud), 0, 0, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(20, 10, utf8_decode('Fecha fin: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
         $pdf->Cell(33, 10, utf8_decode($fechaSolicitudF), 0, 0, 'L');
-        $pdf->Cell(18, 10, 'Hora fin: ', 0, 0, 'L');
-        $pdf->Cell(0, 10, utf8_decode($horaSolicitudF), 0, 1, 'L');
+        $pdf->SetFont('Arial', 'B', 12);
+        $pdf->Cell(18, 10, utf8_decode('Hora fin: '), 0, 0, 'L');
+        $pdf->SetFont('Arial', '', 12);
+        $pdf->Cell(0, 10, utf8_decode($horaSolicitudF), 0, 0, 'L');
     }
-
 
     // Header
     function Header()
