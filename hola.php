9829244b6a52fd1ff782874141dbec929e062c96
Cifu2002 <estebancifu88@gmail.com>
2024-08-26 14:36:23 -0500

    6

--- hola.php
+++ hola.php
@@ -1,11 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>hola mundo</h1>
-</body>
-</html>
+<?php
+include_once('ReportePDF.php');
+$solicitudes = [
+    [
+        'solicitudID' => 60,
+        'codigo' => '9.1.1.17.07.00017.00007',
+        'mac' => '50-EB-F6-D2-F2-67',
+        'tipoSolicitud' => 'Correctiva',
+        'tipoMantenimientoString' => '',
+        'responsableBien' => 'Diana quevedo',
+        'departamento' => 'BODEGA',
+        'encargado' => 'KATHERINE STEFANIA LADINES GARCES',
+        'fechaSolicitud' => '21-08-2024',
+        'horaSolicitud' => '16:04',
+        'fechaSolicitudF' => '22-08-2024',
+        'horaSolicitudF' => '16:04',
+        'detalles' => 'sd',
+        'impresoraString' => '',
+        'componentes' => [
+            'Componente 1',
+            'Componente 2'
+        ],
+        'cambios' => [
+            'Cambio 1',
+            'Cambio 2'
+        ]
+    ],
+    [
+        'solicitudID' => 61,
+        'codigo' => '9.1.1.17.07.00017.00008',
+        'mac' => '50-EB-F6-D2-F2-68',
+        'tipoSolicitud' => 'Preventiva',
+        'tipoMantenimientoString' => 'Mant. de rutina',
+        'responsableBien' => 'Carlos Pérez',
+        'departamento' => 'ADMINISTRACIÓN',
+        'encargado' => 'JUAN PÉREZ',
+        'fechaSolicitud' => '23-08-2024',
+        'horaSolicitud' => '10:00',
+        'fechaSolicitudF' => '24-08-2024',
+        'horaSolicitudF' => '12:00',
+        'detalles' => 'Revisión general',
+        'impresoraString' => 'Impresora XYZ'
+    ]
+];
+
+// Generar el reporte PDF para todas las solicitudes
+PDF::GenerarReportePDF($solicitudes);
+?>
