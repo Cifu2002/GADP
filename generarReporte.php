b1a6ae6be02e143038f7d8f737558114b08d71ee
Cifu2002 <estebancifu88@gmail.com>
2024-08-26 15:21:14 -0500

    6

--- generarReporte.php
+++ generarReporte.php
@@ -2,38 +2,36 @@
 
 include_once('ReportePDF.php');
 
-            $solicitudes = [
-                60 => [
-                    'solicitudID' => 60,
-                    'codigo' => '9.1.1.17.07.00017.00007',
-                    'mac' => '50-EB-F6-D2-F2-67',
-                    'tipoSolicitud' => 'Correctiva',
-                    'tipoMantenimientoString' => '', // Campo vacío
-                    'responsableBien' => 'Diana Quevedo',
-                    'departamento' => 'BODEGA',
-                    'encargado' => 'KATHERINE STEFANIA LADINES GARCES',
-                    'fechaSolicitud' => '21-08-2024',
-                    'horaSolicitud' => '16:04',
-                    'fechaSolicitudF' => '22-08-2024',
-                    'horaSolicitudF' => '16:04',
-                    'detalles' => 'sd',
-                    'impresoraString' => '', // Campo vacío
-                    'componentes' => [
-                        'componente 3',
-                        'componente 2',
-                        'componente 1'
-                    ],
-                    'cambios' => [
-                        'fsdf',
-                        'dsfsdf'
-                    ]
-                ]
-            ];
-            echo "<pre>";
-            print_r($solicitudes);
-            echo "</pre>";
-            
-            PDF::GenerarReportePDF($solicitudes);
-            
-      
+$solicitudes = [
+    60 => [
+        'solicitudID' => 60,
+        'codigo' => '9.1.1.17.07.00017.00007',
+        'mac' => '50-EB-F6-D2-F2-67',
+        'tipoSolicitud' => 'Correctiva',
+        'tipoMantenimientoString' => '', // Campo vacío
+        'responsableBien' => 'Diana Quevedo',
+        'departamento' => 'BODEGA',
+        'encargado' => 'KATHERINE STEFANIA LADINES GARCES',
+        'fechaSolicitud' => '21-08-2024',
+        'horaSolicitud' => '16:04',
+        'fechaSolicitudF' => '22-08-2024',
+        'horaSolicitudF' => '16:04',
+        'detalles' => 'sd',
+        'impresoraString' => '', // Campo vacío
+        'componentes' => [
+            'componente 3',
+            'componente 2',
+            'componente 1'
+        ],
+        'cambios' => [
+            'fsdf',
+            'dsfsdf'
+        ]
+    ]
+];
+
+
+PDF::GenerarReportePDF($solicitudes);
+
+
 ?>
